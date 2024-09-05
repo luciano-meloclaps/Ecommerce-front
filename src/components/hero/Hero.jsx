ab31644f3b2af7bef3789067143672b1243f7cb6
luciano-meloclaps <meloclapsluciano@gmail.com>
2024-09-04 22:34:02 -0300

    Fixed/Img

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -12,14 +12,13 @@ const Hero = () => {
           style={{ height: "100vh" }}
         >
           <Col className="d-flex p-0 align-items-center justify-content-center">
-            <div className="hero-image">
+            <div className="hero-image p-0 m-0">
               <img
-                src="https://images.pexels.com/photos/2964779/pexels-photo-2964779.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                src="src\assets\pexels-rachel-claire-5490198.jpg"
                 alt="Imagen del Hero"
                 style={{
                   width: "100%",
                   height: "100vh",
-                  filter: "blur(1px)",
                   opacity: 0.9,
                 }}
               />
